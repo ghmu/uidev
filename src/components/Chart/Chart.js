730b6c1ff63cf364a65fe103576f7bd4b84d753e
kal-uidev <rjkalpana@yahoo.com>
2020-05-22 16:50:13 -0700

    added initial setup and card section

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chart = (props) => {
+    return(
+    <h1>Chart{props.confirmed}</h1>
+        
+    
+
+    )
+}
+export default Chart;
---
+++ src/components/CountryPicker/CountryPicker.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CountryPicker = () => {
+    return(
+        <h1>Country dropdown</h1>
+    )
+}
+export default CountryPicker;
